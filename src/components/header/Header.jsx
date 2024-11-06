63fffc24abe2dfde59361ab5a9396510cca65d3d
João Avelino <joaolavelino@gmail.com>
2024-11-06 17:29:44 -0300

    test commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,6 +5,7 @@ function Header({}) {
   return (
     <header className={styles.header}>
       <h1>
+        oi
         <img src={logo} alt="two overlapping triangles pointing up-right" />
       </h1>
     </header>
